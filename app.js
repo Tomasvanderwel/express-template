5696cae9838735809394bbf94413839a4cdbf44a
Tomas <tomas.vanderwel@acgedu.com>
2018-07-01 20:07:31 +1200

    cleanup and logging

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 // App Settings
+
 require('dotenv').load();
 
 // App Modules
+
 var express = require('express');
 var path = require('path');
 var morgan = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-// var favicon = require('serve-favicon');
-var winston = require('winston');
+var favicon = require('serve-favicon');
 var util = require('./modules/util/basic');
 
-// Logging Setup
-
 // Express Setup
+
 var port = util.parseInt(process.env.PORT);
 var app = express();
 if (process.env.NODE_ENV === 'dev') app.use(morgan('dev'));
@@ -21,18 +21,18 @@ else app.use(morgan('tiny'));
 // app.set('views', './views');
 // app.set('view engine', 'pug');
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(favicon(path.join(__dirname, 'public', '')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
 
 // Route setup
+
 var index = require('./routes/index');
 app.use('./routes', index);
 
 // Handlers
+
 app.use(function (req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
@@ -46,7 +46,7 @@ app.use(function (err, req, res) {
 
 app.set('port', port);
 var server = app.listen(app.get('port'), function () {
-    return console.log('Express server listening on port ' + server.address().port);
+    return util.verbose('Server listening on port ' + server.address().port);
 });
 
 module.exports = server;
--- modules/util/basic.js
+++ modules/util/basic.js
@@ -1,16 +1,100 @@
+var moment = require('moment-timezone');
+
+var LOG_LEVEL;
+var TIMEZONE = process.env.TIMEZONE;
+
+/**
+ * Sets logging level
+ * @returns {null} null
+ */
+function setLogLevel() {
+    var levelString = process.env.LOG_LEVEL;
+    switch (levelString) {
+    case 'error': return LOG_LEVEL = 1;
+    case 'warning': return LOG_LEVEL = 2;
+    case 'verbose': return LOG_LEVEL = 3;
+    default:
+        if (process.env.NODE_ENV === 'production') return LOG_LEVEL = 0;
+        else return LOG_LEVEL = 3;
+    }
+}
+setLogLevel();
+
+/**
+ * Logs to console if environment supports supplied level
+ * @param {Number} level logging level
+ * @example 1 Error
+ * 2 Warning
+ * 3 Verbose
+ * @param {any} message data string
+ * @returns {null} null
+ */
+function log(level, message) {
+    if (level <= LOG_LEVEL) {
+        var ts = moment().tz(TIMEZONE).format();
+        switch (level) {
+        case 1: return console.log({ error: message, ts: ts });
+        case 2: return console.log({ warning: message, ts: ts });
+        case 3: return console.log({ verbose: message, ts: ts });
+        }
+    }
+}
+
+/**
+ * Logs error to console if possible
+ * @param {any} message data string
+ * @returns {null} null
+ */
+function error(message) {
+    return log(1, message);
+}
+
+/**
+ * Logs warning to console if possible
+ * @param {any} message data string
+ * @returns {null} null
+ */
+function warning(message) {
+    return log(2, message);
+}
+
+/**
+ * Logs verbose to console if possible
+ * @param {any} message data string
+ * @returns {null} null
+ */
+function verbose(message) {
+    return log(3, message);
+}
+
+/**
+ * Tries to parse a value as an integer
+ * @param {any} value input value
+ * @returns {Number|any} integer or input value
+ */
 function tryParseInt(value) {
     var n = Number.parseInt(value);
     if (n) return n;
+    warning(value + ' could not be parsed as an integer');
     return value;
 }
 
+/**
+ * Parses a value as an integer
+ * @param {any} value input value
+ * @returns {Number|null} integer or null
+ */
 function parseInt(value) {
     var n = Number.parseInt(value);
     if (n) return n;
-    return console.error(value + ' could not be parsed');
+    return error(value + ' could not be parsed as an integer');
 }
 
 module.exports = {
+    error: error,
+    log: log,
     parseInt: parseInt,
-    tryParseInt: tryParseInt
+    tryParseInt: tryParseInt,
+    verbose: verbose,
+    warning: warning
 };
